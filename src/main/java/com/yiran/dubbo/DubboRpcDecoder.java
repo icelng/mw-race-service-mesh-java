20fcf55124b11b74ca8a22b6e637e97c84855310
yiran_fancier <ip28@qq.com>
2018-05-14 01:50:44 +0800

    修改dubbo解码

--- src/main/java/com/yiran/dubbo/DubboRpcDecoder.java
+++ src/main/java/com/yiran/dubbo/DubboRpcDecoder.java
@@ -1,37 +1,46 @@
 package com.yiran.dubbo;
 
-import com.yiran.dubbo.model.Bytes;
+import com.yiran.agent.Bytes;
 import com.yiran.dubbo.model.RpcResponse;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class DubboRpcDecoder extends ByteToMessageDecoder {
     // header length.
     protected static final int HEADER_LENGTH = 16;
+    private Boolean isHeader = true;
+    private long requestId;
+    private int dataLength;
 
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) {
-       list.add(decode2(byteBuf));
-    }
+        if (isHeader) {
+            if (byteBuf.readableBytes() < HEADER_LENGTH) {
+                return;
+            }
+            /*解析报头*/
+            byte[] header = new byte[HEADER_LENGTH];
+            requestId = Bytes.bytes2long(header, 4);
+            dataLength = Bytes.bytes2int(header, 12);
 
-    private Object decode2(ByteBuf byteBuf){
-        byte[] data = new byte[byteBuf.readableBytes()];
-        byteBuf.readBytes(data);
+            isHeader = false;
+        }
 
-        byte[] subArray = Arrays.copyOfRange(data,HEADER_LENGTH + 1, data.length);
-
-        String s = new String(subArray);
-
-        byte[] requestIdBytes = Arrays.copyOfRange(data,4,12);
-        long requestId = Bytes.bytes2long(requestIdBytes,0);
-
+        if (byteBuf.readableBytes() < dataLength) {
+            return;
+        }
         RpcResponse response = new RpcResponse();
         response.setRequestId(String.valueOf(requestId));
-        response.setBytes(subArray);
-        return response;
+        byte[] data = new byte[dataLength];
+        byteBuf.readBytes(data, 0, dataLength);
+        response.setBytes(data);
+
+        list.add(response);
+
+        isHeader = true;
     }
+
 }

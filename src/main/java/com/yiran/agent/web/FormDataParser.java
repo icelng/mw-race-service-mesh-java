a68197ba0dd19984781a8787236a06c37fca5cfd
yiran_fancier <ip28@qq.com>
2018-05-17 23:40:45 +0800

    使用netty实现http服务器，测试

--- src/main/java/com/yiran/agent/web/FormDataParser.java
+++ src/main/java/com/yiran/agent/web/FormDataParser.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -52,9 +53,12 @@ public class FormDataParser implements ByteProcessor {
                 parameterMap.put(key, value);
             }
             if (i == -1) {
-                buffer.readerIndex(oldReaderIndex);  // 恢复buffer
                 buffer.writerIndex(buffer.writerIndex() - 1);
-                logger.info("Parse segments:{}", k);
+                if (k != 8) {
+                    buffer.readerIndex(oldReaderIndex);  // 恢复buffer
+                    logger.info("Parse segments:{}", k);
+                    logger.info("The request body is:{}", buffer.toString(Charset.forName("utf-8")));
+                }
                 return parameterMap;
             }
             buffer.readerIndex(i + 1);

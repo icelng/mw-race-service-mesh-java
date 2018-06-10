7c930031ae4893042eb52865461d4824404bb5dc
yiran_fancier <ip28@qq.com>
2018-06-10 16:04:21 +0800

    不使用netty提供的解码器

--- src/main/java/com/yiran/agent/ConsumerAgentEncoder.java
+++ src/main/java/com/yiran/agent/ConsumerAgentEncoder.java
@@ -10,16 +10,21 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 public class ConsumerAgentEncoder extends MessageToByteEncoder<AgentServiceRequest> {
+    private static final int HEADER_LENGTH = 12;
     private static Logger logger = LoggerFactory.getLogger(ConsumerAgentEncoder.class);
+    private byte[] header = new byte[HEADER_LENGTH];
 
     protected void encode(ChannelHandlerContext ctx, AgentServiceRequest msg, ByteBuf out) throws Exception {
 
         //logger.info("Request reqId:{},  data:{}", msg.getRequestId(), msg.getData().toString(CharsetUtil.UTF_8));
         /*写requestId*/
-        out.writeLong(msg.getRequestId());
+        Bytes.long2bytes(msg.getRequestId(), header, 0);
+        //out.writeLong(msg.getRequestId());
         /*写入数据长度*/
-        out.writeInt(msg.getData().readableBytes());
+        Bytes.int2bytes(msg.getData().readableBytes(), header, 8);
+        //out.writeInt(msg.getData().readableBytes());
         /*写入数据*/
+        out.writeBytes(header);
         out.writeBytes(msg.getData());
 
     }

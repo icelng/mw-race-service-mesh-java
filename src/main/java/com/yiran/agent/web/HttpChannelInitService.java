888f8ec912c73e718c28b989bb13f43d10d314cd
yiran_fancier <ip28@qq.com>
2018-06-13 15:47:47 +0800

    100ms读超时关闭,去除100ms的写超时

--- src/main/java/com/yiran/agent/web/HttpChannelInitService.java
+++ src/main/java/com/yiran/agent/web/HttpChannelInitService.java
@@ -20,7 +20,7 @@ public class HttpChannelInitService extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel sc)
             throws Exception {
-        sc.pipeline().addLast(new IdleStateHandler(100, 100, 0, TimeUnit.MILLISECONDS));
+        sc.pipeline().addLast(new IdleStateHandler(100, 0, 0, TimeUnit.MILLISECONDS));
 
         sc.pipeline().addLast(new HttpResponseEncoder());
 

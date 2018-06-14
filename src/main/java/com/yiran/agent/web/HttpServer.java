f3a43e6a146b0f190d91eb06102318a9b5ea9dd4
yiran_fancier <ip28@qq.com>
2018-06-15 01:17:32 +0800

    io改为3

--- src/main/java/com/yiran/agent/web/HttpServer.java
+++ src/main/java/com/yiran/agent/web/HttpServer.java
@@ -25,7 +25,7 @@ public class HttpServer {
 
     public void run() throws Exception {
         EventLoopGroup bossGroup = new EpollEventLoopGroup(1);
-        EventLoopGroup workerGroup = new EpollEventLoopGroup(4);
+        EventLoopGroup workerGroup = new EpollEventLoopGroup(3);
         EventLoopGroup agentClientWorkerGroup = new EpollEventLoopGroup(4);
 
         AgentClientManager agentClientManager = new AgentClientManager(agentClientWorkerGroup);

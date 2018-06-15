fc8ab553ed031477f0ec568eb5aa6ae4aa231f2f
yiran_fancier <ip28@qq.com>
2018-06-15 15:17:55 +0800

    1k写限制

--- src/main/java/com/yiran/agent/web/HttpChannelInitService.java
+++ src/main/java/com/yiran/agent/web/HttpChannelInitService.java
@@ -21,7 +21,7 @@ public class HttpChannelInitService extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel sc)
             throws Exception {
-        GlobalTrafficShapingHandler globalTrafficShapingHandler = new GlobalTrafficShapingHandler(sc.eventLoop().parent(), 512, 1024*1024);
+        GlobalTrafficShapingHandler globalTrafficShapingHandler = new GlobalTrafficShapingHandler(sc.eventLoop().parent(), 1024, 1024*1024);
         globalTrafficShapingHandler.configure(0);
 
 //        sc.pipeline().addLast(new IdleStateHandler(100, 0, 0, TimeUnit.MILLISECONDS));

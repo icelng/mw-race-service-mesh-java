2eeecf32d163682f71ee0ab8b0f2362026b7127c
yiran_fancier <ip28@qq.com>
2018-06-16 17:02:47 +0800

    响应速率=请求速率-200

--- src/main/java/com/yiran/LoadBalance.java
+++ src/main/java/com/yiran/LoadBalance.java
@@ -178,13 +178,13 @@ public class LoadBalance {
 
     public void calRequestRate() {
 
-        if (requestRateCalPeriod.addAndGet(1) == 512) {
+        if (requestRateCalPeriod.addAndGet(1) == 256) {
             long intervalNanoTime = System.nanoTime() - lastNanoTime;
             lastNanoTime += intervalNanoTime;
-            requestRate = (float) 512 / (float) intervalNanoTime * 1000000000;
-            if (requestRate > 8000) {
-                logger.info("The request rate(QPS:{}) is higher than 8000", requestRate);
-            }
+            requestRate = (float) 256 / (float) intervalNanoTime * 1000000000;
+            //if (requestRate > 8000) {
+            //    logger.info("The request rate(QPS:{}) is higher than 8000", requestRate);
+            //}
             requestRateCalPeriod.set(0);
         }
 
